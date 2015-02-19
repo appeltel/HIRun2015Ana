25d68060fb8ce2c5d2c67b57ba789e96c574ef05
Eric <eric.appelt@gmail.com>
2015-02-19 13:49:09 -0600

    There is no good reason that I can see to have separate ntuple definitions in the sim->reco and reco->sim trees. The same information is contained in each ntuple, only the # of matched tracks/particles has a different meaning.
    The helper class will just use one generic ntuple for both trees.

--- interface/HITrackCorrectionTreeHelper.h
+++ interface/HITrackCorrectionTreeHelper.h
@@ -14,15 +14,18 @@
 // Branches for track correction analysis ntuples
 
 // Simulated Particle (one) -> Reco Track (many)
+// or
+// Reco Track (one) -> Simulated Particle (many)
+//
 // only best match is stored
 
 typedef struct
 {
-  // PDG id, and status (i.e. -99 is secondary)
+  // sim particle PDG id, and status (i.e. -99 is secondary)
   Int_t ids;
   Int_t status;
 
-  // kinematics
+  // simulated particle kinematics
   Int_t charges;
   Float_t pts;
   Float_t etas;
@@ -31,10 +34,10 @@ typedef struct
   // G4SimHits of simulated particle
   Int_t hits;
 
-  // number of matched tracks
-  Int_t nrec;
+  // number of matched tracks or particles
+  Int_t nmatch;
 
-  // kinematics of the matched track
+  // kinematics of the reco track
   Int_t charger;
   Float_t ptr;
   Float_t etar;
@@ -44,71 +47,23 @@ typedef struct
   Float_t etaerr;
   Float_t phierr;
 
-  // vertex compatibility of matched track
+  // vertex compatibility of reco track
   Float_t d0;
   Float_t d0err;
   Float_t dz;
   Float_t dzerr;
 
-  // reconstructed hits of matched track and goodness of fit
+  // reconstructed hits of reco track and goodness of fit
   Int_t hitr;
   Float_t chi2r;
 
-  // Iteration of matched track
+  // Iteration of reco track
   Int_t algo;
 
   // Event centrality bin
   Int_t cbin;
 
-} HISimTrackBranches_t;
-
-// Reco track (one) -> simulated particle (many)
-// only best match is stored
-
-typedef struct
-{
-
-  // reco track kinematics
-  Int_t charger;
-  Float_t ptr;
-  Float_t etar;
-  Float_t phir;
-  Float_t pterr;
-  Float_t etaerr;
-  Float_t phierr;
-
-  // vertex compatibility
-  Float_t d0;
-  Float_t d0err;
-  Float_t dz;
-  Float_t dzerr;
-
-  // number of hits and chi2
-  Int_t hitr; 
-  Float_t chi2r;
-
-  // iteration
-  Int_t algo;
-
-  // number of matched simulated particles
-  Int_t nsim;
-
-  // status and pdgid of matched particle
-  Int_t ids;
-  Int_t status;
-
-  // kinematics of matched particle
-  Float_t pts;
-  Float_t etas;
-  Float_t phis;
-
-  // G4SimHits of matched particle
-  Int_t hits;
-
-  // centrality bin of event
-  Int_t cbin;
-
-} HIRecTrackBranches_t;
+} HITrackBranches_t;
 
 
 // Helper class for producing track correction ntuples
@@ -117,17 +72,13 @@ class HITrackCorrectionTreeHelper
 {
   public:
 
-    HIRecTrackBranches_t r;
-    HISimTrackBranches_t s;    
+    HITrackBranches_t b;
 
-    TString rLeafString;
-    TString sLeafString;
+    TString hiTrackLeafString;
   
-    void SetRec(const reco::Track &, const reco::TrackingParticle &, const reco::Vertex &);
-    void SetRec(const reco::Track &, const reco::TrackingParticle &, const reco::Vertex &, double );
+    void Set(const reco::Track &, const reco::TrackingParticle &, const reco::Vertex &);
+    void Set(const reco::Track &, const reco::TrackingParticle &, const reco::Vertex &, double );
 
-    void SetSim(const reco::Track &, const reco::TrackingParticle &, const reco::Vertex &);
-    void SetSim(const reco::Track &, const reco::TrackingParticle &, const reco::Vertex &, double );
 };
 
 #endif 
