b6f9450bb2c214a359698817047835c423cbdde7
Eric <eric.appelt@gmail.com>
2015-03-06 18:46:34 -0600

    Add analyzer class for digging deeper into track reconstruction layer by layer, starting by modifying the HITrackCorrectionAnalyzer. Right now it is just a bunch of couts, but should develop into histograms for the readouts of each module that the particle traverses.

---
+++ test/HITrackProfiler_test_cfg.py
@@ -0,0 +1,73 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('TRACKANA')
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.ReconstructionHeavyIons_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Appeltel.HIRun2015Ana.HITrackProfiler_cfi')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(5)
+)
+
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('trackProfiler.root')
+)
+
+process.load("SimTracker.TrackAssociation.trackingParticleRecoTrackAsssociation_cfi")
+
+process.tpRecoAssocGeneralTracks = process.trackingParticleRecoTrackAsssociation.clone()
+process.tpRecoAssocGeneralTracks.label_tr = cms.InputTag("hiGeneralTracks")
+
+process.load("SimTracker.TrackAssociatorProducers.quickTrackAssociatorByHits_cfi")
+process.quickTrackAssociatorByHits.SimToRecoDenominator = cms.string('reco')
+process.quickTrackAssociatorByHits.Cut_RecoToSim = 0.02
+process.quickTrackAssociatorByHits.Quality_SimToReco = 0.02
+process.quickTrackAssociatorByHits.Purity_SimToReco = 0.02
+
+process.load("SimTracker.TrackerHitAssociation.clusterTpAssociationProducer_cfi")
+
+# Input source
+process.source = cms.Source("PoolSource",
+    duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+    fileNames =  cms.untracked.vstring(
+'/store/user/mnguyen/PyquenUnquenched_Dijet_pthat80_740pre6_GEN-SIM/PyquenUnquenched_Dijet_pthat80_740pre6_MCHI2_74_V0_RECO/35189fe3832afd77f4f2029b245352e2/step3_RAW2DIGI_L1Reco_RECO_100_1_0f3.root'
+    )
+)
+
+
+#centrality
+process.load("RecoHI.HiCentralityAlgos.CentralityBin_cfi") 
+process.centralityBin.Centrality = cms.InputTag("hiCentrality")
+process.centralityBin.centralityVariable = cms.string("HFtowers")
+process.centralityBin.nonDefaultGlauberModel = cms.string("HydjetDrum5")
+process.GlobalTag.toGet.extend([
+   cms.PSet(record = cms.string("HeavyIonRcd"),
+      tag = cms.string("CentralityTable_HFtowers200_HydjetDrum5_v740x01_mc"),
+      connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS"),
+      label = cms.untracked.string("HFtowersHydjetDrum5")
+   ),
+])
+process.hiTrackProfiler.useCentrality = True
+process.hiTrackProfiler.centralitySrc = cms.InputTag("centralityBin","HFtowers")
+
+
+process.hiTrackProfiler.applyTrackCuts = False
+process.hiTrackProfiler.fillNTuples = True
+
+process.GlobalTag.globaltag = 'MCHI2_74_V0::All'
+
+process.p = cms.Path( 
+                      process.tpClusterProducer *
+                      process.quickTrackAssociatorByHits *
+                      process.tpRecoAssocGeneralTracks *
+                      process.centralityBin *
+                      process.hiTrackProfiler
+)
