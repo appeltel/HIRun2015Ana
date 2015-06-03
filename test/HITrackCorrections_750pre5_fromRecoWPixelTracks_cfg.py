a34f552c6ae2f3c1630f8787eaaa3eb683a678e2
Eric <eric.appelt@gmail.com>
2015-06-03 10:24:56 -0500

    add cfg for 750pre5 corrections

---
+++ test/HITrackCorrections_750pre5_fromRecoWPixelTracks_cfg.py
@@ -0,0 +1,65 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('TRACKANA')
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.ReconstructionHeavyIons_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Appeltel.HIRun2015Ana.HITrackCorrectionAnalyzer_cfi')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('trackCorrections.root')
+)
+
+process.load("SimTracker.TrackAssociation.trackingParticleRecoTrackAsssociation_cfi")
+
+process.tpRecoAssocGeneralTracks = process.trackingParticleRecoTrackAsssociation.clone()
+process.tpRecoAssocGeneralTracks.label_tr = cms.InputTag("hiGeneralAndPixelTracks")
+
+process.load("SimTracker.TrackAssociatorProducers.quickTrackAssociatorByHits_cfi")
+process.quickTrackAssociatorByHits.SimToRecoDenominator = cms.string('reco')
+
+process.load("SimTracker.TrackerHitAssociation.clusterTpAssociationProducer_cfi")
+
+# Input source
+process.source = cms.Source("PoolSource",
+    duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+    fileNames =  cms.untracked.vstring(
+'file:step3.root'
+    )
+)
+
+
+#centrality
+#process.load("RecoHI.HiCentralityAlgos.CentralityBin_cfi") 
+#process.centralityBin.Centrality = cms.InputTag("hiCentrality")
+#process.centralityBin.centralityVariable = cms.string("HFtowers")
+#process.centralityBin.nonDefaultGlauberModel = cms.string("HydjetDrum5")
+
+process.HITrackCorrections.useCentrality = False
+process.HITrackCorrections.centralitySrc = cms.InputTag("centralityBin","HFtowers")
+process.HITrackCorrections.trackSrc = cms.InputTag("hiGeneralAndPixelTracks")
+
+process.HITrackCorrections.applyTrackCuts = False
+process.HITrackCorrections.fillNTuples = False
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc_HIon', '')
+
+process.p = cms.Path( 
+                      process.tpClusterProducer *
+                      process.quickTrackAssociatorByHits *
+                      process.tpRecoAssocGeneralTracks *
+                  #    process.centralityBin *
+                      process.HITrackCorrections
+)
